4a90483927ffbed08daf7700b9512e961f44c5a6
PetarBabic <petar.babich@icloud.com>
2026-09-11 20:56:42 +0300

    Expanding functionallity: the program now reads the camera settings (exposure length and gain) from a csv file, encodes the images with proper metadata and when the code is run, it'll take pictures untill stopped. Store them inside the following folder: Images/CurrentDate/CurrentTime.jpg

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # All-sky Camera
 
 Continuation of an all-sky camera project at Aalto University
+
+## Set-up and Requirments 
+It is recommended to use Python version 3.12.
+
+The easiest way to install would be to clone this repo and execute the following commands:
+```
+python3.12 -m venv <repo>
+
+cd <repo>
+source bin/activate
+
+pip -r requirements.txt
+```
--- camera_control.py
+++ camera_control.py
@@ -2,17 +2,79 @@ import cv2
 import pyzwoasi
 from pyzwoasi import ZWOCamera
 from pyzwoasi.pyzwoasi import ASIImageType
+import csv
+import exif
+from datetime import datetime
+import os
+
+image_metadata = {
+    # 'camera_owner_name': 'Aalto University',
+    'copyright': 'Aalto University',
+    'datetime': '',
+    'exposure_time': 0,
+    'f_number': '1.2',
+    'focal_length': '2.5',
+    'gain': 0,
+    # 'temperature': '',
+    'model': 'ZWO ASI676MC'
+}
+
+def read_settings(): # type: ignore
+    with open("camera_settings.csv", "r") as f:
+        data = csv.DictReader(f)
+        for row in data:
+            image_metadata['exposure_time'] = float(row['exposure'])
+            image_metadata['gain'] = int(row['gain'])
+
+# https://exif.readthedocs.io/en/latest/usage.html
+def encode_image(img, filename):
+    status, image_jpg_coded = cv2.imencode('.jpg', img)
+
+    image_jpg_coded_bytes = image_jpg_coded.tobytes()
+    exif_jpg = exif.Image(image_jpg_coded_bytes)
+
+    for name, attr in image_metadata.items():
+        exif_jpg[name] = attr
+
+    filename = filename + '/' +datetime.now().strftime("%H%M-%S") + ".jpg"
+
+    with open(filename, 'wb') as new_image_file:
+        new_image_file.write(exif_jpg.get_file())
 
 
-numOfConnectedCameras = pyzwoasi.getNumOfConnectedCameras()
-if (numOfConnectedCameras == 0):
-    print("No camera connected")
-    exit()
+def camera_set_settings(camera: ZWOCamera):
+    settings = read_settings()
 
-for cameraIndex in range(numOfConnectedCameras):
-    with ZWOCamera(cameraIndex) as camera:
-        camera.imageType = ASIImageType.ASI_IMG_RGB24
+    camera.exposure = int(image_metadata['exposure_time'] * 1e6)
+    camera.gain = image_metadata['gain']    
 
-        camera.gain = 70
-        im = camera.shot(exposureTime_us = 50000)
-        cv2.imwrite('image.jpg', im)
+def take_picture():
+    path = 'Images'
+
+    if(not os.path.isdir(path)):
+        os.mkdir(path)
+
+    path = path + '/' + str(datetime.today().strftime('%Y-%m-%d'))
+    if(not os.path.isdir(path)):
+        os.mkdir(path)
+
+    numOfConnectedCameras = pyzwoasi.getNumOfConnectedCameras()
+    if (numOfConnectedCameras == 0):
+        print("No camera connected")
+        exit()
+
+    for cameraIndex in range(numOfConnectedCameras):
+        with ZWOCamera(cameraIndex) as camera:
+            camera.imageType = ASIImageType.ASI_IMG_RGB24
+
+            camera_set_settings(camera)
+
+            camera.gain = 70
+            image_metadata['datetime'] = str(datetime.now())
+            im = camera.shot()
+
+            encode_image(im, path)
+
+if __name__ == "__main__":
+    while(True):
+        take_picture()
